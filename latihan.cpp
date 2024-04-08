820ea96262142259ae523b152650cd74fa15d943
FalzProject <naufalsatya10@gmail.com>
2024-04-08 11:08:58 +0700

    practice1

---
+++ latihan.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+Buatlah program yang menampilkan :
+a. Nama anda
+b. Nim anda
+c. Jurusan anda
+d. Asal
+*/
+
+int main () {
+	cout << "===========\n";
+	cout << "Hello World\n";
+	cout << "===========\n";
+	cout << "Nama 		: Naufal Satya Pradananto\n";
+	cout << "NIM 		: 23.61.0253\n";
+	cout << "Jurusan 	: Bachelor of Informatics\n";
+	cout << "Asal		: Yogyakarta\n";
+}
